350dfe6398658879ba430b7a613006ea24063141
Vibhor Meshram <80259000+meshramvibhor@users.noreply.github.com>
2022-01-15 11:26:51 +0530

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,7 @@ import {Link} from "react-router-dom";
 const NavBar = ()=> {
     return (
       <div>
-        <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-light" >
+        <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark" >
           <div className="container-fluid">
             <Link className="navbar-brand" to="/home">
               News Wala
